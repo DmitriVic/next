4a74337c2095b88c7e351cf6e889c1c2a560add5
DmitriVic <Mr_vlasov@mail.ru>
2023-02-03 21:18:17 +0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { fchmod } from 'fs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -121,3 +122,4 @@ export default function Home() {
     </>
   )
 }
+
